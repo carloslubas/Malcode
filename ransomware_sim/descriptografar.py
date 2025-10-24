70dbea3d1742b0a0a9fdd9fd70facf7ae60e950c
Carlos Alberto <87396518+carloslubas@users.noreply.github.com>
2025-10-24 08:51:17 -0400

    Update descriptografar.py v1

--- ransomware_sim/descriptografar.py
+++ ransomware_sim/descriptografar.py
@@ -12,4 +12,21 @@ def descriptografar_arquivo(arquivo,chave):
   with open(arquivo, "wb") as file:
     file.write(dados_descriptografados)
 
+def encontrar_arquivos(diretorio):
+    lista[]
+    for raiz, _, arquivos in os.walk(diretorio):
+        for nome in arquivos:
+            caminho = os.path.join(raiz, nome)
+            if nome != "rasomware.pyw" and not nome.endswith(".key"):
+                lista.append(caminho)
+    return lista
 
+def main():
+  chave = carregar_chave()
+  arquivo = encontrar_arquivos("Malcode/ransomware_sim")
+  for arquivo in arquivos:
+    descriptografar_arquivo(arquivo, chave)
+  print("Arquivos restaurados com sucesso")
+
+if __name__ == "__main__"
+  main()
