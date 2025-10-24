2ccdb829ddd3891caa467fb4ee5a243dd853caa4
Carlos Alberto <87396518+carloslubas@users.noreply.github.com>
2025-10-24 08:44:05 -0400

    Update descriptografar.py

--- ransomware_sim/descriptografar.py
+++ ransomware_sim/descriptografar.py
@@ -1,1 +1,15 @@
+from cryptograpy.fernet import Fernet
+import os
+
+def carregar_chave():
+  return open("chave.key", "rb").read()
+
+def descriptografar_arquivo(arquivo,chave):
+  f = Fernet(chave)
+  with open(arquivo, "rb") as file:
+    dados = file.read()
+    dados_descriptografados = f.decrypt(dados)
+  with open(arquivo, "wb") as file:
+    file.write(dados_descriptografados)
+
 
