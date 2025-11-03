30ea34b7afef67d1bee3c57011acac7c4fdf17da
Carlos Alberto <87396518+carloslubas@users.noreply.github.com>
2025-11-03 14:40:21 -0400

    Update ransoware.py

--- ransomware_sim/ransoware.py
+++ ransomware_sim/ransoware.py
@@ -2,7 +2,7 @@ from cryptography.fernet import Fernet
 import os
 
 #1. Gerar uma chave de criptografia e salvar
-def gener_chave():
+def gerar_chave():
     chave= Fernet.generate_key()
     with open("chave.key", "wb") as chave_file:
       chave_file.write(chave)
